2229a8e4d06231f0eb90815ab41ec3e360410123
Mike Filonov <mikefilonov@gmail.com>
2017-07-10 02:42:13 +0000

    added arch flag so the 64 image can be donwloaded for testing

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-VERSION=$1
-IMAGE=$2
+ARCH=$1
+VERSION=$2
+IMAGE=$3
 
-wget -O- get.pharo.org/${VERSION} | bash
+if [ $ARCH = "64" ]; then
+	wget -O- get.pharo.org/64/${VERSION} | bash
+else
+	wget -O- get.pharo.org/${VERSION} | bash
+fi
+
 docker run -ti --rm -v `pwd`:/var/data "$IMAGE" pharo /var/data/Pharo.image test '^Z.*'
