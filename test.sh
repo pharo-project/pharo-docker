a19921eaf3a703a139d27a07b6c50387bf79fa67
Mike Filonov <mikefilonov@gmail.com>
2017-07-10 02:58:41 +0000

    changed test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,18 @@ VERSION=$2
 IMAGE=$3
 
 if [ $ARCH = "64" ]; then
-	wget -O- get.pharo.org/64/${VERSION} | bash
+	echo wget -O- get.pharo.org/64/${VERSION} | bash
 else
-	wget -O- get.pharo.org/${VERSION} | bash
+	echo wget -O- get.pharo.org/${VERSION} | bash
 fi
 
-docker run -ti --rm -v `pwd`:/var/data "$IMAGE" pharo /var/data/Pharo.image test '^Z.*'
+# a copy from https://github.com/pharo-project/pharo-vm/blob/master/scripts/run-tests.sh
+NO_TEST="^(?!Metacello)"		# too long
+NO_TEST="$NO_TEST(?!Versionner)"	# too long
+NO_TEST="$NO_TEST(?!GT)"		# too slow
+#NO_TEST="$NO_TEST(?!Athens)"		# no cairo, no athens
+#NO_TEST="$NO_TEST(?!OSWindow)"		# no cairo, no oswindow
+#NO_TEST="$NO_TEST(?!TxText)"		# no cairo, no TxText
+NO_TEST="$NO_TEST(?!ReleaseTests)"	# just not now :)
+
+docker run -ti --rm -v `pwd`:/var/data "$IMAGE" pharo /var/data/Pharo.image test --no-xterm --fail-on-failure "$NO_TEST[A-Z].*"
