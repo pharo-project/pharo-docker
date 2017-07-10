695289d98d6f6ae579c6e910b9dc423b1e0a4ec3
Mike FIlonov <mikefilonov@gmail.com>
2017-07-09 22:42:45 -0700

    Configure base Docker templates and CI (#1)
    * Added dependencies
    * Added tests.sh to run tests in CI
    ** Only basic tests for stable images (Kernel-Tests for base, Athens for libcairo test, Zodiac for ssl 
    ** No testing for unstable 70 branch

--- test.sh
+++ test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-VERSION=$1
-IMAGE=$2
+ARCH=$1
+VERSION=$2
+IMAGE=$3
 
-wget -O- get.pharo.org/${VERSION} | bash
-docker run -ti --rm -v `pwd`:/var/data "$IMAGE" pharo /var/data/Pharo.image test '^Z.*'
+if [ $ARCH = "64" ]; then
+	wget -O- get.pharo.org/64/${VERSION} | bash
+else
+	wget -O- get.pharo.org/${VERSION} | bash
+fi
+
+# Just the basics to ensure image is alright
+TEST="(Nothing)"
+if [ $VERSION -le 60 ] && [ $ARCH -eq 32 ]; then
+	TEST="$TEST|(Kernel-Tests)"
+fi
+TEST="$TEST|(Athens.*)"
+TEST="$TEST|(Zodiac-Tests)"
+
+docker run -ti --rm -v `pwd`:/var/data "$IMAGE" pharo /var/data/Pharo.image test --no-xterm --fail-on-failure "${TEST}"
